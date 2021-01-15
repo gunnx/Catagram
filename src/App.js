71b29aa1b55e5adcc2aae9518d3dc76a55db9854
Keith Gunn <kexith@gmail.com>
2021-01-15 15:29:48 +0000

    setup boilerplate project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-family: Arial, sans-serif;
+  font-size: 5rem;
+  text-align: center;
+  padding: 0;
+  margin: 2rem 0;
+  color: chocolate;
+`
+
+const App = () => {
+  return (
+    <Title>
+      Hello from Catagram!!!
+    </Title>
+  )
+}
+
+export default App
---
+++ webpack.common.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+      inject: true
+    })
+  ]
+}
